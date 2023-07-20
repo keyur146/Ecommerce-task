3b2c3f27c3c482e405f85b742b615a6567642114
Keyur146 <keyurbchauhan@gmail.com>
2023-07-20 16:51:27 +0530

    20/7/2023

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const env = require("./config/envConfig");
+const connect = require("./config/db");
+const userRoutes = require("./routes/users/userRoutes")
+const app = express();
+
+// database connection
+
+connect();
+
+// add middleware
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.json({msg: 'Welcome to chawkbazar!'});
+});
+
+// user routes
+
+app.use('/api', userRoutes);
+
+const port = env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Your server is running at port number: ${port}`);
+})
